48acd76b41067f304c0524d9e4b62e2e7073dabc
Teemu Erkkola <teemu.erkkola@gofore.com>
2021-05-09 17:34:45 +0300

    message server error handling

--- message_server/src/main.rs
+++ message_server/src/main.rs
@@ -2,35 +2,24 @@ use message_io::network::{NetEvent, Transport};
 use message_io::node::{self};
 use std::collections::HashMap;
 
+use anyhow;
 use chatrs::{ClientMessage, ServerMessage};
 
 struct Client {
     nick: String,
 }
 
-fn main() {
-    // Create a node, the main message-io entity. It is divided in 2 parts:
-    // The 'handler', used to make actions (connect, send messages, signals, stop the node...)
-    // The 'listener', used to read events from the network or signals.
+fn main() -> anyhow::Result<()> {
     let (handler, listener) = node::split::<()>();
 
-    // Listen for TCP, UDP and WebSocket messages at the same time.
     handler
         .network()
-        .listen(Transport::FramedTcp, "0.0.0.0:3042")
-        .unwrap();
-    handler
-        .network()
-        .listen(Transport::Udp, "0.0.0.0:3043")
-        .unwrap();
-    handler
-        .network()
-        .listen(Transport::Ws, "0.0.0.0:3044")
-        .unwrap();
+        .listen(Transport::FramedTcp, "0.0.0.0:3042")?;
+    handler.network().listen(Transport::Udp, "0.0.0.0:3043")?;
+    handler.network().listen(Transport::Ws, "0.0.0.0:3044")?;
 
     let mut clients = HashMap::new();
 
-    // Read incoming network events.
     listener.for_each(move |event| match event.network() {
         NetEvent::Connected(endpoint, _) => {
             clients.insert(
@@ -42,31 +31,39 @@ fn main() {
             println!("Client connected");
         }
         NetEvent::Message(endpoint, data) => {
-            let client_message = ClientMessage::deserialize(&data).unwrap();
-            match client_message {
-                ClientMessage::Message { content } => {
-                    let nick = if let Some(client) = clients.get(&endpoint) {
-                        client.nick.clone()
-                    } else {
-                        "unknown".to_owned()
-                    };
-                    let message = ServerMessage::Message { nick, content }
-                        .serialize()
-                        .unwrap();
-                    for client in clients.keys() {
-                        handler.network().send(*client, &message);
+            if let Ok(client_message) = ClientMessage::deserialize(&data) {
+                match client_message {
+                    ClientMessage::Message { content } => {
+                        let nick = if let Some(client) = clients.get(&endpoint) {
+                            client.nick.clone()
+                        } else {
+                            "unknown".to_owned()
+                        };
+                        let message = ServerMessage::Message { nick, content };
+                        if let Ok(data) = message.serialize() {
+                            for client in clients.keys() {
+                                handler.network().send(*client, &data);
+                            }
+                        } else {
+                            eprintln!("ERROR: a serialization error occurred");
+                        }
                     }
-                }
-                ClientMessage::Nick { nick } => {
-                    if let Some(client) = clients.get_mut(&endpoint) {
-                        client.nick = nick;
+
+                    ClientMessage::Nick { nick } => {
+                        if let Some(client) = clients.get_mut(&endpoint) {
+                            client.nick = nick;
+                        }
                     }
-                }
-            };
+                };
+            } else {
+                eprintln!("ERROR: a deserialization error occurred");
+            }
         }
         NetEvent::Disconnected(endpoint) => {
             clients.remove(&endpoint);
             println!("Client disconnected");
         }
     });
+
+    Ok(())
 }
